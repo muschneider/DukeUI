1aa10ad38464cd479980ea98a476c96bfaf8515a
Mauro Schneider <maurous@gmail.com>
2015-01-09 10:38:56 -0200

    added ratpack 0.9.11

--- src/ratpack/ratpack.groovy
+++ src/ratpack/ratpack.groovy
@@ -1,14 +1,20 @@
+
+
+import ratpack.form.Form
+import ratpack.groovy.templating.TemplatingModule
+
+import static ratpack.groovy.Groovy.groovyTemplate
 import static ratpack.groovy.Groovy.ratpack
 
 ratpack {
-    handlers {
-        get("foo") {
-            render "from the foo handler"
-        }
-        get("bar") {
-            render "from the bar handler"
-        }
 
-        assets "public", "index.html"
+	bindings {
+        add(TemplatingModule) { TemplatingModule.Config config -> config.staticallyCompile = true }
+    }
+
+    handlers {
+		get("run") {
+			render groovyTemplate("run.html", title: "Data Matching")
+		}
     }
 }
